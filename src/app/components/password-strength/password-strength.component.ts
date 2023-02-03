707cc6c20ff0cdbc66822c3f2eafba43253972e4
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-02-03 19:13:39 +0200

    add some styles

--- src/app/components/password-strength/password-strength.component.ts
+++ src/app/components/password-strength/password-strength.component.ts
@@ -10,13 +10,14 @@ export class PasswordStrengthComponent {
   inputValue = '';
   color = ''
   passwordStrength = ''
+  message = ''
 
   colors = {
       'invalid': 'rgb(208, 201, 201)',
-      'weak': 'rgb(205, 44, 44)',
-      'easy': 'rgb(205, 44, 44)',
-      'medium': 'rgb(248, 236, 14)',
-      'strong': 'rgb(40, 158, 40)',
+      'weak': 'rgb(218, 79, 79)',
+      'easy': 'rgb(218, 79, 79)',
+      'medium': 'rgb(240, 231, 59)',
+      'strong': 'rgb(58, 185, 58)',
     }
 
   handleChange(event: any): void {
@@ -36,14 +37,17 @@ export class PasswordStrengthComponent {
     const hasCharacters = specialCharacters.test(password);
 
     if (password.length === 0) {
+      this.message = ''
       return "invalid";
     }
 
-    if ( password && password.length < 8) {
+    if (password && password.length < 8) {
+      this.message = 'Your password is less than 8 symbols'
       return 'weak';
     }
 
     if (hasLetters && hasDigits && hasCharacters) {
+      this.message = `Your password is strong. You needn't be afraid of`
       return 'strong';
     }
 
@@ -51,10 +55,12 @@ export class PasswordStrengthComponent {
       (hasLetters && hasCharacters) ||
       (hasDigits && hasCharacters)
     ) {
+      this.message = "Your password is medium. You should add letters, digits and special characters to make your password strong"
       return 'medium';
     }
 
     if (hasLetters || hasDigits || hasCharacters) {
+      this.message = "Your password is too easy. You should add letters, digits and special characters to make your password strong"
       return 'easy';
     }
 
